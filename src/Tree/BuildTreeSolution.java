e49db889756de84daefac36e68722c78e05812bb
tgomudur@asu.edu <tgomudur@asu.edu>
2016-12-12 16:09:26 -0700

    Added TreeBuilder from Inorder and Postorder

--- src/Tree/BuildTreeSolution.java
+++ src/Tree/BuildTreeSolution.java
@@ -17,18 +17,34 @@ class TreeBuilder<T extends Comparable<T>>{
     public BinarySearchTreeNode<T> buildTreeFromPreInOrder(T[] preorder, T[] inorder) {
         if (inorder.length == 0 || preorder.length == 0) return null;
         Map<T, Integer> inMap = mapIndices(inorder);
-        return buildTree(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1, inMap);
+        return buildTreeFromPreInOrder(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1, inMap);
     }
 
-    private BinarySearchTreeNode<T> buildTree(T[] preorder, int preStart, int preEnd, T[] inorder, int inStart, int inEnd, Map<T, Integer> inMap){
+    private BinarySearchTreeNode<T> buildTreeFromPreInOrder(T[] preorder, int preStart, int preEnd, T[] inorder, int inStart, int inEnd, Map<T, Integer> inMap){
         if (inStart > inEnd || preStart > preEnd) return null;
         T rootVal = preorder[preStart];
         BinarySearchTreeNode<T> root = new BinarySearchTreeNode<T>(rootVal);
         int inIdx = inMap.get(rootVal);
         int numsInLST = inIdx - inStart;
-        root.setLeft(buildTree(preorder, preStart + 1, preStart + numsInLST, inorder, inStart, inIdx - 1, inMap));
-        root.setRight(buildTree(preorder, preStart + numsInLST + 1, preEnd, inorder, inIdx + 1, inEnd, inMap));
+        root.setLeft(buildTreeFromPreInOrder(preorder, preStart + 1, preStart + numsInLST, inorder, inStart, inIdx - 1, inMap));
+        root.setRight(buildTreeFromPreInOrder(preorder, preStart + numsInLST + 1, preEnd, inorder, inIdx + 1, inEnd, inMap));
+        return root;
+    }
 
+    public BinarySearchTreeNode<T> buildTreeFromPostInOrder(T[] postorder, T[] inorder) {
+        if (inorder.length == 0 || postorder.length == 0) return null;
+        Map<T, Integer> inMap = mapIndices(inorder);
+        return buildTreeFromPostInOrder(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1, inMap);
+    }
+
+    private BinarySearchTreeNode<T> buildTreeFromPostInOrder(T[] inorder, int inStart, int inEnd, T[] postorder, int postStart, int postEnd, Map<T, Integer> inMap){
+        if (inStart > inEnd || postStart > postEnd) return null;
+        T rootVal = postorder[postEnd];
+        BinarySearchTreeNode<T> root = new BinarySearchTreeNode<T>(rootVal);
+        int inIdx = inMap.get(rootVal);
+        int numsInLST =  inIdx - inStart;
+        root.setLeft(buildTreeFromPostInOrder(inorder, inStart, inIdx - 1, postorder, postStart, postStart + numsInLST - 1, inMap));
+        root.setRight(buildTreeFromPostInOrder(inorder, inIdx + 1, inEnd, postorder,  postStart + numsInLST, postEnd - 1, inMap));
         return root;
     }
 }
@@ -46,20 +62,22 @@ public class BuildTreeSolution{
 
         List<Integer> inOrderList = bst.inOrder();
         List<Integer> preOrderList = bst.preOrder();
+        List<Integer> postOrderList = bst.postOrder();
+
         int numOfElements = inOrderList.size();
 
         Integer[] preOrderArray = (Integer[]) preOrderList.toArray(new Integer[0]);
         Integer[] inOrderArray = (Integer[]) inOrderList.toArray(new Integer[0]);
-//        int[] preOrderArray = new int[preOrderList.size()];
-//        int[] inOrderArray = new int[inOrderList.size()];
-//
-//        for(int i = 0; i < inOrderList.size(); i++){
-//            inOrderArray[i] = inOrderList.get(i);
-//            preOrderArray[i] = preOrderList.get(i);
-//        }
-        BinarySearchTreeNode<Integer> root = tb.buildTreeFromPreInOrder(preOrderArray,inOrderArray);
+        Integer[] postOrderArray = (Integer[]) postOrderList.toArray(new Integer[0]);
+
+//        BinarySearchTreeNode<Integer> root = tb.buildTreeFromPreInOrder(preOrderArray,inOrderArray);
+        BinarySearchTreeNode<Integer> root = tb.buildTreeFromPostInOrder(postOrderArray,inOrderArray);
+
         BinarySearchTree newbst = new BinarySearchTree<Integer>(root, numOfElements);
-        System.out.println(newbst.postOrder());
+
+        System.out.println(newbst.postOrder().equals(postOrderList));
+
+
 
     }
     }
