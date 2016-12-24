f9bc5d7c34b879924b0dd94260a562a376f5526e
tgomudur@asu.edu <tgomudur@asu.edu>
2016-12-23 23:14:04 -0600

    Added buls and cow game stub

---
+++ src/HashMap/BullsAndCows.java
@@ -0,0 +1,13 @@
+package HashMap;
+
+/**
+ * Created by incyphae10 on 12/23/16.
+ * Problem description : https://leetcode.com/problems/bulls-and-cows/
+ */
+class BullsAndCowsSolution {
+    public String getHint(String secret, String guess) {
+        //TODO: Implement get hint
+    }
+}
+public class BullsAndCows {
+}
