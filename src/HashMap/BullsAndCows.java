2d775e0e4887200b1ea83c44036c17246829487f
tgomudur@asu.edu <tgomudur@asu.edu>
2016-12-24 23:45:37 -0600

    Added main func to solution to bulls and cow game

--- src/HashMap/BullsAndCows.java
+++ src/HashMap/BullsAndCows.java
@@ -29,25 +29,25 @@ class BullsAndCowsSolution {
 
     // One pass. Single array
     public String getHint(String secret, String guess) {
-        public String getHint(String secret, String guess) {
-            int[] countArray = new int[10];
-            char c;
-            int bulls = 0;
-            int cows = 0;
-            for (int i = 0; i < secret.length(); i++){
-                if (secret.charAt(i) == guess.charAt(i)) bulls++;
-                else{
-                    if (countArray[guess.charAt(i) - '0'] > 0) cows++;
-                    if (countArray[secret.charAt(i) - '0'] < 0) cows++;
-                    countArray[guess.charAt(i) - '0']--;
-                    countArray[secret.charAt(i) - '0']++;
-                }
+        int[] countArray = new int[10];
+        char c;
+        int bulls = 0;
+        int cows = 0;
+        for (int i = 0; i < secret.length(); i++){
+            if (secret.charAt(i) == guess.charAt(i)) bulls++;
+            else{
+                if (countArray[guess.charAt(i) - '0'] > 0) cows++;
+                if (countArray[secret.charAt(i) - '0'] < 0) cows++;
+                countArray[guess.charAt(i) - '0']--;
+                countArray[secret.charAt(i) - '0']++;
             }
-            return bulls + "A" + cows + "B";
         }
+        return bulls + "A" + cows + "B";
     }
 }
 public class BullsAndCows {
-    BullsAndCowsSolution s = new BullsAndCowsSolution();
-    System.out.println(s.getHint("1234", "0111"));
+    public static void main (String[] args){
+        BullsAndCowsSolution s = new BullsAndCowsSolution();
+        System.out.println(s.getHint("1234", "0111"));
+    }
 }
