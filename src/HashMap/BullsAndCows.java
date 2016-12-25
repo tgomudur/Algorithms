1a450f4067cd41b8bd9084cf37d1c82187d19762
tgomudur@asu.edu <tgomudur@asu.edu>
2016-12-24 23:41:09 -0600

    Added solution to bulls and cow game

--- src/HashMap/BullsAndCows.java
+++ src/HashMap/BullsAndCows.java
@@ -5,8 +5,26 @@ package HashMap;
  * Problem description : https://leetcode.com/problems/bulls-and-cows/
  */
 class BullsAndCowsSolution {
+
     public String getHint(String secret, String guess) {
-        //TODO: Implement get hint
+        int[] secretArr = new int[10];
+        int[] guessArr = new int[10];
+
+        char c;
+        int bulls = 0;
+        int cows = 0;
+        for (int i = 0; i < secret.length(); i++){
+            if (secret.charAt(i) == guess.charAt(i)) bulls++;
+            else{
+                guessArr[guess.charAt(i) - '0']++;
+                secretArr[secret.charAt(i) - '0']++;
+            }
+        }
+
+        for (int i = 0; i < secretArr.length; i++){
+            cows += Math.min(secretArr[i], guessArr[i]);
+        }
+        return bulls + "A" + cows + "B";
     }
 }
 public class BullsAndCows {
