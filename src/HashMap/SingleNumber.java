e392726c009ffd6013298f8d3e1e02f8ae2a5b7f
tgomudur@asu.edu <tgomudur@asu.edu>
2016-12-26 23:21:56 -0600

    Added XOR solution to single number problem

--- src/HashMap/SingleNumber.java
+++ src/HashMap/SingleNumber.java
@@ -2,11 +2,12 @@ package HashMap;
 
 /**
  * Created by tharun on 12/26/16.
+ * Problem description: https://leetcode.com/problems/single-number/
  */
 
 public class SingleNumberSolution {
     // Time complexity: O(n), Space Complexity: O(n)
-    public int singleNumber(int[] nums) {
+    public int singleNumber2(int[] nums) {
         HashSet<Integer> hs = new HashSet<Integer>();
         for (int num : nums){
             if (hs.contains(num)) hs.remove(num);
@@ -15,7 +16,15 @@ public class SingleNumberSolution {
         if (hs.size() != 0) return hs.iterator().next();
         return 0;
     }
+
+    // XOR solution. Time complexity: O(n), Space complexity: O(1)
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) result = result ^ num;
+        return result;
+    }
 }
 
 public class SingleNumber {
+    
 }
