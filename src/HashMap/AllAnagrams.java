6494d38016790996589128ddd5637041856a02a3
tgomudur@asu.edu <tgomudur@asu.edu>
2016-12-27 23:48:22 -0600

    Added hashmap solution to all anagrams problem.

---
+++ src/HashMap/AllAnagrams.java
@@ -0,0 +1,50 @@
+package HashMap;
+
+/**
+ * Created by tharun on 12/27/16.
+ * Problem description: https://leetcode.com/problems/find-all-anagrams-in-a-string/
+ */
+
+class AllAnagramSolution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<Integer>();
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        HashMap<Character, Integer> pMap = new HashMap<Character, Integer>();
+
+        int n = s.length();
+        int m = p.length();
+        int left = 0, right = 0;
+        char c;
+
+        if (m > n) return result;
+
+        for (int i = 0; i < m; i++){
+            c = p.charAt(i);
+            if (pMap.containsKey(c)) pMap.put(c, pMap.get(c) + 1);
+            else pMap.put(c, 1);
+        }
+
+        while (left <= n - m){
+            while (right - left < m){
+                c = s.charAt(right);
+                if (map.containsKey(c)) map.put(c, map.get(c) + 1);
+                else map.put(c, 1);
+                right++;
+            }
+
+            if (map.equals(pMap))
+                result.add(left);
+
+            map.put(s.charAt(left), map.get(s.charAt(left)) - 1);
+
+            if (map.get(s.charAt(left)) == 0)
+                map.remove(s.charAt(left));
+            left++;
+
+        }
+        return result;
+    }
+}
+
+public class AllAnagrams {
+}
