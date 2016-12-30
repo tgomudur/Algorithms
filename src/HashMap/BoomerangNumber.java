0f99986bcb0ea6cb1dc07c5bc5d8061f0f36f6ee
tgomudur@asu.edu <tgomudur@asu.edu>
2016-12-29 23:39:17 -0600

    Added boomerang number problem

---
+++ src/HashMap/BoomerangNumber.java
@@ -0,0 +1,18 @@
+package HashMap;
+
+/**
+ * Created by tharun on 12/29/16.
+ * Problem description : https://leetcode.com/problems/number-of-boomerangs/
+ */
+class BommerangNumberSolution {
+    public int numberOfBoomerangs(int[][] points) {
+        // TODO: Implement solution
+    }
+}
+public class BoomerangNumber {
+    public static void main (String[] args) {
+        BommerangNumberSolution s = new BommerangNumberSolution();
+        int[][] points = {{0,0},{1,0},{2,0}};
+        s.numberOfBoomerangs(points);
+    }
+}
