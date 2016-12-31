1b34fd8aef3c449f4185dfa49c3c2d2fc53e9c21
tgomudur@asu.edu <tgomudur@asu.edu>
2016-12-30 23:31:32 -0600

    Added O(n) space complexity solution to happy number problem

---
+++ src/HashMap/HappyNumber.java
@@ -0,0 +1,35 @@
+package HashMap;
+
+/**
+ * Created by tharun on 12/30/16.
+ * Problem description: https://leetcode.com/problems/happy-number/
+ */
+
+class HappyNumberSolution {
+    public int calculateSquareSum(int n){
+        int totalSum = 0, digit = 0;
+        while (n > 0){
+            digit = n % 10;
+            totalSum += digit*digit;
+            n = n/10;
+        }
+        return totalSum;
+    }
+
+    // O(n) Space Complexity
+    public boolean isHappy(int n) {
+        HashSet<Integer> seen = new HashSet<Integer>();
+        int squareSum = calculateSquareSum(n);
+        while (seen.add(squareSum) && squareSum != 1) {
+            squareSum = calculateSquareSum(squareSum);
+        }
+        return (squareSum == 1);
+    }
+}
+
+public class HappyNumber {
+    public static void main (String[] args){
+        HappyNumberSolution s = new HappyNumberSolution();
+        System.out.println(s.isHappy(7));
+    }
+}
