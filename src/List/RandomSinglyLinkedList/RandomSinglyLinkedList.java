ea0df98e10024980cbdf4c593ab8a3bbd1b1f796
tgomudur@asu.edu <tgomudur@asu.edu>
2017-01-01 18:44:06 -0700

    Added classes for RandomSLL

---
+++ src/List/RandomSinglyLinkedList/RandomSinglyLinkedList.java
@@ -0,0 +1,7 @@
+package List.RandomSinglyLinkedList;
+
+/**
+ * Created by incyphae10 on 1/1/17.
+ */
+public class RandomSinglyLinkedList {
+}
---
+++ src/List/RandomSinglyLinkedList/RandomSinglyLinkedListNode.java
@@ -0,0 +1,10 @@
+package List.RandomSinglyLinkedList;
+
+/**
+ * Created by incyphae10 on 1/1/17.
+ */
+public class RandomSinglyLinkedListNode {
+    int label;
+    RandomSinglyLinkedListNode next, random;
+    RandomSinglyLinkedListNode(int x) { this.label = x; }
+}
