dc9589a9acf5568822835c5ade1050c9377bc0fc
tgomudur@asu.edu <tgomudur@asu.edu>
2017-01-05 17:59:29 -0700

    Added solution to converting sorted array to bst

---
+++ src/Tree/SortedArrayToBST.java
@@ -0,0 +1,23 @@
+package Tree;
+
+/**
+ * Created by tharun on 1/5/17.
+ */
+
+public class SortedArrayToBST {
+    public BinarySearchTreeNode<Integer> sortedArrayToBST(int[] nums) {
+        if (nums.length == 0) return null;
+        return sortedArrayToBSTHelper(nums, 0, nums.length - 1);
+    }
+
+    public BinarySearchTreeNode<Integer> sortedArrayToBSTHelper(int[] nums, int left, int right) {
+        if (left > right) return null;
+
+        int mid = (left + right)/2;
+        BinarySearchTreeNode<Integer> root = new BinarySearchTreeNode<Integer>(nums[mid]);
+        root.left = sortedArrayToBSTHelper(nums, left, mid - 1);
+        root.right = sortedArrayToBSTHelper(nums, mid + 1, right);
+
+        return root;
+    }
+}
