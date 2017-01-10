9f959a56056cfa5a5fc851d984107a4e12e87c78
tgomudur@asu.edu <tgomudur@asu.edu>
2017-01-10 04:29:18 -0700

    Designed mini twitter

--- src/HashMap/DesignTwitter.java
+++ src/HashMap/DesignTwitter.java
@@ -3,67 +3,119 @@ package HashMap;
 /**
  * Created by tharun on 1/9/17.
  */
+
 import java.util.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+class Tweet {
+    int tweetId;
+    int time;
+    Tweet next;
+
+    public Tweet(int tweetId, int timestamp) {
+        this.tweetId = tweetId;
+        time = timestamp++;
+        next = null;
+    }
+}
+
+class User {
+    int userId;
+    HashSet<Integer> follows;
+    Tweet tweetHead;
+
+    public User(int userId) {
+        this.userId = userId;
+        this.follows = new HashSet<>();
+        this.tweetHead = null;
+        follows.add(userId);
+    }
+
+    public void post(Tweet newTweet) {
+        // Add latest tweet to the front.
+        newTweet.next = tweetHead;
+        tweetHead = newTweet;
+    }
+
+    public void follow(int userId) {
+        follows.add(userId);
+    }
+
+    public void unfollow(int userId){
+        follows.remove(new Integer(userId));
+    }
+}
+
 class Twitter {
-    HashMap<Integer, HashSet<Integer>> userTweetsMap;
-    HashMap<Integer, HashSet<Integer>> followersMap;
-    HashMap<Integer, ArrayList<Integer>> newsFeedMap;
+    static int timestamp = 0;
+    HashMap<Integer, User> usersMap;
 
     /** Initialize your data structure here. */
     public Twitter() {
-        userTweetsMap = new HashMap<>();
-        followersMap = new HashMap<>();
-        newsFeedMap = new HashMap<>();
+        usersMap = new HashMap<>();
     }
 
     /** Compose a new tweet. */
     public void postTweet(int userId, int tweetId) {
-        // Add tweet to user tweets map
-        if (!userTweetsMap.containsKey(userId)) userTweetsMap.put(userId, new HashSet<Integer>());
-        userTweetsMap.get(userId).add(tweetId);
+        Tweet newTweet = new Tweet(tweetId, timestamp++);
 
-        // Add tweetId to front of news feed of userId
-        if (!newsFeedMap.containsKey(userId)) newsFeedMap.put(userId, new ArrayList<Integer>());
-        newsFeedMap.get(userId).add(0, tweetId);
-
-        // Add tweetId to front of all userId's followers
-        if (!followersMap.containsKey(userId)) followersMap.put(userId, new HashSet<Integer>());
-        else {
-            for (int followerId : followersMap.get(userId)) {
-                if (!newsFeedMap.containsKey(followerId)) newsFeedMap.put(followerId, new ArrayList<Integer>());
-                // Remove the last tweet is size == 10, before adding new tweet to news feed.
-                if (newsFeedMap.get(followerId).size() == 10) newsFeedMap.get(followerId).remove(newsFeedMap.get(followerId).size() - 1);
-                newsFeedMap.get(followerId).add(0, tweetId);
-            }
-        }
-        System.out.println(followersMap);
+        if (!usersMap.containsKey(userId)) usersMap.put(userId, new User(userId));
+        usersMap.get(userId).post(newTweet);
     }
 
-    /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
+    /** Retrieve the 10 most recent tweet ids in the user's news feed.
+     * Each item in the news feed must be posted by users who the user followed or by the user herself.
+     * Tweets must be ordered from most recent to least recent. */
     public List<Integer> getNewsFeed(int userId) {
-        return newsFeedMap.get(userId);
+        List<Integer> feedList = new LinkedList<>();
+
+        if (!usersMap.containsKey(userId)) return feedList;
+
+        PriorityQueue<Tweet> minHeap = new PriorityQueue<>(10, new Comparator<Tweet>() {
+            @Override
+            public int compare(Tweet t1, Tweet t2) {
+                return t2.time - t1.time;
+            }
+        });
+
+        for (int followerId : usersMap.get(userId).follows) {
+            User user = usersMap.get(followerId);
+            // System.out.println(usersMap.get(userId).follows);
+            if (user.tweetHead != null)
+                minHeap.add(user.tweetHead);
+        }
+
+
+        Tweet tmpTweet;
+        while (!minHeap.isEmpty() && feedList.size() < 10) {
+            tmpTweet = minHeap.poll();
+            if (tmpTweet != null)
+                feedList.add(tmpTweet.tweetId);
+
+            if (tmpTweet.next != null) {
+                minHeap.add(tmpTweet.next);
+            }
+        }
+
+        return feedList;
     }
 
     /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
     public void follow(int followerId, int followeeId) {
-        if (!followersMap.containsKey(followeeId)) followersMap.put(followeeId, new HashSet<Integer>());
-        followersMap.get(followeeId).add(followerId);
+        if (!usersMap.containsKey(followerId))
+            usersMap.put(followerId, new User(followerId));
+
+        if (!usersMap.containsKey(followeeId))
+            usersMap.put(followeeId, new User(followeeId));
+
+        usersMap.get(followerId).follow(followeeId);
     }
 
     /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
     public void unfollow(int followerId, int followeeId) {
-        if (followersMap.containsKey(followeeId)){
-            followersMap.get(followeeId).remove(followerId);
-
-            if (newsFeedMap.containsKey(followerId)) {
-                ArrayList<Integer> currentUserFeed = new ArrayList<>(newsFeedMap.get(followerId));
-                for (int tweetId : currentUserFeed) {
-                    if (userTweetsMap.containsKey(followeeId) && userTweetsMap.get(followeeId).contains(tweetId))
-                        newsFeedMap.get(followerId).remove(new Integer(tweetId));
-                }
-            }
-
-        }
+        if (usersMap.containsKey(followerId) && followerId != followeeId)
+            usersMap.get(followerId).unfollow(followeeId);
     }
 }
 
@@ -77,32 +129,31 @@ class Twitter {
  */
 
 public class DesignTwitter {
-    public static void main (String[] args) {
+    @Test
+    public void testTwitter() {
         Twitter twitter = new Twitter();
 
         // User 1 posts a new tweet (id = 5).
-        twitter.postTweet(1, 1);
+        twitter.postTweet(1, 5);
 
         // User 1's news feed should return a list with 1 tweet id -> [5].
-        System.out.println(twitter.getNewsFeed(1));
+        assertEquals(Arrays.asList(5), twitter.getNewsFeed(1));
 
         // User 1 follows user 2.
-        twitter.follow(2, 1);
-
-        System.out.println(twitter.getNewsFeed(2));
+        twitter.follow(1, 2);
 
         // User 2 posts a new tweet (id = 6).
-//        twitter.postTweet(2, 6);
+        twitter.postTweet(2, 6);
 
         // User 1's news feed should return a list with 2 tweet ids -> [6, 5].
         // Tweet id 6 should precede tweet id 5 because it is posted after tweet id 5.
-//        System.out.println(twitter.getNewsFeed(1));
+        assertEquals(Arrays.asList(6, 5), twitter.getNewsFeed(1));
 
         // User 1 unfollows user 2.
-        twitter.unfollow(2, 1);
+        twitter.unfollow(1, 2);
 
         // User 1's news feed should return a list with 1 tweet id -> [5],
         // since user 1 is no longer following user 2.
-        System.out.println(twitter.getNewsFeed(2));
+        assertEquals(Arrays.asList(5), twitter.getNewsFeed(1));
     }
 }
