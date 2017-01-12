46b702aa958e50a28ce88e0cc7cf59adaec7f9c0
tgomudur@asu.edu <tgomudur@asu.edu>
2017-01-12 16:48:45 -0700

    Desgined data structure called RandomizedSet to support insert, delete, getRandom in O(1) time.

---
+++ src/HashMap/RandomizedSetTest.java
@@ -0,0 +1,70 @@
+package HashMap;
+
+/**
+ * Created by tharun on 1/12/17.
+ * Problem description : https://leetcode.com/problems/insert-delete-getrandom-o1/
+ * No duplicates allowed.
+ */
+
+class RandomizedSet {
+    HashMap<Integer, Integer> map;
+    ArrayList<Integer> keyList;
+    Random rand;
+
+
+    /** Initialize your data structure here. */
+    public RandomizedSet() {
+        map = new HashMap<>();
+        keyList = new ArrayList<>();
+        rand = new Random();
+    }
+
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+        if (!map.containsKey(val)) {
+            map.put(val, keyList.size());
+            keyList.add(val);
+            return true;
+        }
+        return false;
+    }
+
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+        // Deleting tail in DLL is O(1).
+        // Copy last element value to current val's position and deleting last position.
+        if (map.containsKey(val)) {
+            // Copy and update
+            int currentValIdx = map.get(val);
+            keyList.set(currentValIdx, keyList.get(keyList.size() - 1));
+            map.put(keyList.get(keyList.size() - 1), currentValIdx);
+            // Remove
+            keyList.remove(keyList.size() - 1);
+            map.remove(val);
+            return true;
+        }
+        return false;
+    }
+
+    /** Get a random element from the set. */
+    public int getRandom() {
+        return keyList.get(rand.nextInt(keyList.size()));
+    }
+}
+
+public class RandomizedSetTest {
+    @Test
+    public void testInsert() {
+
+    }
+
+    @Test
+    public void testRemove() {
+
+    }
+
+    @Test
+    public void testGetRandom() {
+
+    }
+}
