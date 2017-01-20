cd941b4e37d330ffc76395535811cb7391722de4
tgomudur@asu.edu <tgomudur@asu.edu>
2017-01-20 02:57:27 -0700

    Added solution to convert sorted linked list to bst

--- src/Tree/SortedListToBST.java
+++ src/Tree/SortedListToBST.java
@@ -6,8 +6,26 @@ package Tree;
  */
 
 public class SortedListToBST {
+    // Time : O(n) Space : O(1)
     public TreeNode sortedListToBST(ListNode head) {
+        if (head == null) return null;
+        return toBST(head, null);
+    }
 
+    public TreeNode toBST(ListNode head, ListNode tail) {
+        ListNode fast = head;
+        ListNode slow = head;
+        if (slow == tail) return null;
+
+        while (fast != tail && fast.next != tail) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        TreeNode root = new TreeNode(slow.val);
+        root.left = toBST(head, slow);
+        root.right = toBST(slow.next, tail);
+        return root;
     }
 
     // Using hashmap: Time: O(n) Space: O(n)
