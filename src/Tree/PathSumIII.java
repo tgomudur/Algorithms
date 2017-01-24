c46a0264e436570620e9dae808cc35a0c7b1c9b3
tgomudur@asu.edu <tgomudur@asu.edu>
2017-01-23 22:39:22 -0700

    Added O(n) solution to counting paths which equals a given sum in BT

--- src/Tree/PathSumIII.java
+++ src/Tree/PathSumIII.java
@@ -6,10 +6,38 @@ package Tree;
  */
 
 public class PathSumIII {
-    // Time Complexity : O(n^2), Space complexity : O(1)
-    public int findPathSum(BinarySearchTreeNode<Integer> root, int sum) {
+    // Time Complexity : O(n) Space Complexity : O(n)
+    public int findPathSum(TreeNode root, int sum) {
         if (root == null) return 0;
-        return findPathSumFrom(root, sum) + findPathSum(root.left, sum) + findPathSum(root.right, sum);
+
+        // Map <prefixSum, Number of paths to reach this sum>
+        HashMap<Integer, Integer> preSum = new HashMap<>();
+        preSum.put(0, 1);
+        return pathSumHelper(root, 0, sum, preSum);
+    }
+
+    public int pathSumHelper(TreeNode root, int sum, int target, HashMap<Integer, Integer> preSum) {
+        if (root == null) return 0;
+        
+        // Update prefixSum
+        sum += root.val;
+        // Get the number of valid path, ended by the current node
+        int result = preSum.getOrDefault(sum - target, 0);
+
+        // Update the map with the current sum, so the map is good to be passed to the next recursion
+        preSum.put(sum, preSum.getOrDefault(sum, 0) + 1);
+        result += pathSumHelper(root.left, sum, target, preSum) + pathSumHelper(root.right, sum, target, preSum);
+
+        // Restore the map, as the recursion goes from the bottom to the top
+        preSum.put(sum, preSum.get(sum) - 1);
+
+        return result;
+    }
+
+    // Time Complexity : O(n^2), Space complexity : O(1)
+    public int findPathSum1(BinarySearchTreeNode<Integer> root, int sum) {
+        if (root == null) return 0;
+        return findPathSumFrom(root, sum) + findPathSum1(root.left, sum) + findPathSum1(root.right, sum);
     }
 
     public int findPathSumFrom(BinarySearchTreeNode<Integer> root, int sum) {
