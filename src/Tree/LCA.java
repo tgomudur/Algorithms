b9c81e7e7b31262805b963e5c53a5c437d8989d5
tgomudur@asu.edu <tgomudur@asu.edu>
2017-01-25 23:49:07 -0700

    Added problem of finding LCA in BST and BT

---
+++ src/Tree/LCA.java
@@ -0,0 +1,15 @@
+package Tree;
+
+/**
+ * Created by tharun on 1/25/17.
+ * Problem description: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+ */
+public class LCA {
+    public int findLCAinBST(BinarySearchTreeNode<Integer> root) {
+
+    }
+
+    public int findLCAinBT(BinarySearchTreeNode<Integer> root) {
+
+    }
+}
