e39f94abf676cff9a3d1cbd4bf956f3ddf1eff9a
tgomudur@asu.edu <tgomudur@asu.edu>
2017-01-27 22:33:22 -0700

    Bug fix to validating BST

--- src/Tree/BSTValidator.java
+++ src/Tree/BSTValidator.java
@@ -2,17 +2,17 @@ package Tree;
 
 /**
  * Created by tharun on 1/27/17.
+ * Problem description: https://leetcode.com/problems/validate-binary-search-tree/
  */
 
 public class BSTValidator {
-
-    // Time Complexity : O(n)
-    public boolean isValidBST(TreeNode root) {
+    // Time Complexity : O(n) Space Complexity : O(1)
+    public boolean isValidBST(BinarySearchTreeNode<Integer> root) {
         if (root == null) return true;
         return isValidBSTHelper(root, null, null);
     }
 
-    public boolean isValidBSTHelper(TreeNode root, Integer min, Integer max) {
+    public boolean isValidBSTHelper(BinarySearchTreeNode<Integer> root, Integer min, Integer max) {
         if (root == null) return true;
 
         if ((min != null && min >= root.val) || (max != null && max <= root.val))
