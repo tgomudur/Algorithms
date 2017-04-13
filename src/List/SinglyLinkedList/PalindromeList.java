ae306986f4f552518a61eb7d60a88048e9fc7297
tgomudur@asu.edu <tgomudur@asu.edu>
2017-04-12 21:38:27 -0700

    Added comments

--- src/List/SinglyLinkedList/PalindromeList.java
+++ src/List/SinglyLinkedList/PalindromeList.java
@@ -5,7 +5,9 @@ package List.SinglyLinkedList;
  * <a href="https://leetcode.com/problems/palindrome-linked-list/#/solutions">More Info</a>
  */
 public class PalindromeList {
+    // Time Complexity : O(n). Space Complexity: O(1)
     public boolean isPalindrome(SinglyLinkedListNode<Integer> head) {
+        // In this approach, the list is modified and not restored. Could be restored though without increasing complexity.
         SinglyLinkedListNode<Integer> current1 = head;
         SinglyLinkedListNode<Integer> slow = head;
         SinglyLinkedListNode<Integer> fast = head;
