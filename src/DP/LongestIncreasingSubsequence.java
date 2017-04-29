5798dcb6a8853731e12cb2c3411f048db79e97f3
tgomudur@asu.edu <tgomudur@asu.edu>
2017-04-29 11:26:36 -0700

    Added nlogn solution to finding length of longest increasing

--- src/DP/LongestIncreasingSubsequence.java
+++ src/DP/LongestIncreasingSubsequence.java
@@ -1,6 +1,12 @@
 package DP;
 
 /**
+ * Given an unsorted array of integers, find the length of longest increasing subsequence.
+ * For example,
+ * Given [10, 9, 2, 5, 3, 7, 101, 18],
+ * The longest increasing subsequence is [2, 3, 7, 101], therefore the length is 4. Note that there may be more than one LIS combination, it is only necessary for you to return the length.
+ * Your algorithm should run in O(n2) complexity.
+ * Follow up: Could you improve it to O(n log n) time complexity?
  * https://leetcode.com/problems/longest-increasing-subsequence/
  */
 public class LongestIncreasingSubsequence {
@@ -27,6 +33,39 @@ public class LongestIncreasingSubsequence {
                 table[i] += 1;
             }
         }
+    }
+
+    // Time : O(nlogn)
+    // Space : O(n)
+    public void findLengthOfLISEfficient(int[] nums) {
+        int lengthOfLIS = 0;
+        int n = nums.length;
+        if (n != 0) {
+            // tails[i] is the smallest tail element of the LIS of length i.
+            // for each num we do
+            // if (num > all tails), then append it
+            // if (tails[i - 1] < num <= tails[i]), then update tails[i] with num;
+            int[] tails = new int[n];
+            int insertIdx = 0;
+            for (int num : nums) {
+                insertIdx = binarySearch(tails, 0, lengthOfLIS, num);
+                tails[insertIdx] = num;
+                if (insertIdx == lengthOfLIS) lengthOfLIS++;
+            }
+        }
+        return lengthOfLIS;
+    }
+
+    public int binarySearch(int[] arr, int left, int right, int target) {
+        int mid;
+        while (left < right) {
+            mid = (left + right)/2;
+            if (nums[mid] < num)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left;
     }
 }
 
