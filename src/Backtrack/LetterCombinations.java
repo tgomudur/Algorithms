23b28abd19b69fe31715726b4b92ee04139579d1
tgomudur@asu.edu <tgomudur@asu.edu>
2017-05-05 16:16:38 -0700

    Solution to letter combinations in a phone problem

---
+++ src/Backtrack/LetterCombinations.java
@@ -0,0 +1,33 @@
+package Backtrack;
+
+/**
+ * Input:Digit string "23"
+ * Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ * https://leetcode.com/problems/letter-combinations-of-a-phone-number/#/description
+ */
+
+public class LetterCombinations {
+    String[] mapping = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+        if (digits.length() == 0 || digits == null)
+            return result;
+        return findLetterCombinationsHelper(digits, result, new StringBuilder(), 0);
+    }
+
+    public List<String> findLetterCombinationsHelper(String digits, List<String> result, StringBuilder current, int start) {
+        if (current.length() == digits.length()) {
+            result.add(current.toString());
+            return result;
+        }
+
+        String options = mapping[Character.getNumericValue(digits.charAt(start))];
+        for (char option : options.toCharArray()) {
+            findLetterCombinationsHelper(digits, result, current.append(option), start + 1);
+            current.setLength(current.length() - 1);
+        }
+
+        return result;
+    }
+}
