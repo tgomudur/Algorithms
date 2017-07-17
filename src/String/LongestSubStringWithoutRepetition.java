2a70ac6b61cb90cbeece91b3e745513fa0ddf100
Tharun <tgomudur@asu.edu>
2017-07-17 12:02:25 -0700

    Added solution to finding longest substring without repetition

--- src/String/LongestSubStringWithoutRepetition.java
+++ src/String/LongestSubStringWithoutRepetition.java
@@ -4,9 +4,31 @@ import java.util.Map;
 import java.util.HashMap;
 /**
  * Created by tharun on 12/16/16.
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/#/description
  */
 
 class LongestSubStringWithoutRepetitionSolution {
+    // Using hashmap
+    // Time : O(n)
+    // Space : O(k) - where k = min(n, m), n - length of string, m - character space size.
+    public String findLongestSubstringWithoutRepetition2(String s) {
+        int n = s.length();
+        int left = 0, right = 0, maxLength = 0, maxStartIdx;
+        // Maps character to position in array
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        while (left < n && right < n) {
+            if (map.containsKey(s.charAt(right))) {
+                left = Math.max(left, map.get(s.charAt(right)));
+            }
+            if (right - left + 1 > maxLength) {
+                maxLength = right - left + 1;
+                maxStartIdx = left;
+            }
+            map.put(s.charAt(right), right + 1);
+            right++;
+        }
+    }
+
     public String findLongestSubstringWithoutRepetition(String s) {
         Map<Character, Integer> map = new HashMap<Character, Integer>();
         int start = 0;
